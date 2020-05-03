9f0e7fb907801df3aeacf25da3653fd9a49f5aa8
bachrulum20 <ulum424@gmail.com>
2020-05-03 20:14:43 +0800

    Operator percabangan condition1 false, condition2 true

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 78000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 50000;
+        double uangDiDompet = 78000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang belanjaan anda tidak cukup, kurangi belanjaan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double kembalian = uangDiDompet-totalBelanja;
+            System.out.println("Uang anda cukup, kembalian : "+kembalian);
         }else{
-            System.out.println("Uang anda cukup, terimakasih telah berbelanja");
+            System.out.println("Uang anda PAS");
         }
     }
 }
