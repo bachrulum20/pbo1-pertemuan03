f01c0d649af860f44b104956eef3be5634d5fafb
bachrulum20 <ulum424@gmail.com>
2020-05-03 20:22:45 +0800

    Operator percabangan switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,26 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("rengginang");
+                break;
+            case 2:
+                System.out.println("ucing");
+                break;
+            case 3:
+                System.out.println("energen");
+                break;
+            case 4:
+                System.out.println("kulkas 8 pintu");
+                break;
+            default:
+                System.out.println("Anda Ganteng");
+                break;
+
+
+        }
+    }
+}
