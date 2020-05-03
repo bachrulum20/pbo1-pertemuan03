87ea315d46c736ef5e852caaa3f4cff6a0d4bbaf
bachrulum20 <ulum424@gmail.com>
2020-05-03 20:06:41 +0800

    Operator percabangan

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args); {
+        double totalBelanja = 78000;
+        double uangDiDompet = 50000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang belanjaan anda tidak cukup, kurangi belanjaan anda");
+        }else{
+            System.out.println("Uang anda cukup, terimakasih telah berbelanja");
+        }
+    }
+}
